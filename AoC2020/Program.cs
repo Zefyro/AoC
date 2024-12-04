8b2c400ff4231975469e1c9e724638f967fc6938
Zefyro <71092443+Zefyro@users.noreply.github.com>
2024-12-04 23:07:03 +0200

    AoC 2020 - Day 1

---
+++ AoC2020/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC2020;
+public class AoC {
+    public static void Main(string[] args) {
+        Day1(File.ReadAllText("AoC2020/inputs/day1.txt"));
+    }
+    public static void Day1(string input) {
+        Console.WriteLine("\nAdvent Of Code 2020 - Day 1");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+        int[] nums = [.. input.Split('\n').Select(int.Parse)];
+        int product = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = 0; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == 2020) {
+                    product = nums[i] * nums[j];
+                    break;
+                }
+            }
+            if (product != 0) break;
+        }
+
+        Console.WriteLine($"Part 1: {product}");
+
+        product = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = 0; j < nums.Length; j++) {
+                for (int k = 0; k < nums.Length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 2020) {
+                        product = nums[i] * nums[j] * nums[k];
+                        break;
+                    }
+                }
+                if (product != 0) break;
+            }
+            if (product != 0) break;
+        }
+
+        Console.WriteLine($"Part 2: {product}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
--- AoCAll/Program.cs
+++ AoCAll/Program.cs
@@ -4,6 +4,7 @@ public class AoC {
         AoC2015.AoC.Main(args);
         AoC2018.AoC.Main(args);
         AoC2019.AoC.Main(args);
+        AoC2020.AoC.Main(args);
         AoC2023.AoC.Main(args);
         AoC2024.AoC.Main(args);
     }
