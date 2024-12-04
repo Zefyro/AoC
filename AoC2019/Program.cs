d89fe16e9fd9068487e0d5d7969dc92d24873ca1
Zefyro <71092443+Zefyro@users.noreply.github.com>
2024-12-04 22:49:44 +0200

    AoC 2019 - Day 1

---
+++ AoC2019/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AoC2019;
+public class AoC {
+    public static void Main(string[] args) {
+        Day1(File.ReadAllText("AoC2019/inputs/day1.txt"));
+    }
+    public static void Day1(string input) {
+        Console.WriteLine("\nAdvent Of Code 2019 - Day 1");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+        string[] modules = input.Split('\n');
+        int total_fuel = 0;
+        foreach (string fuel in modules)
+            total_fuel += int.Parse(fuel) / 3 - 2;
+        
+        Console.WriteLine($"Part 1: {total_fuel}");
+
+        total_fuel = 0;
+        foreach (string fuel in modules) {
+            int req_fuel = 0;
+            int additional_fuel = int.Parse(fuel) / 3 - 2;
+            while (additional_fuel > 0) {
+                req_fuel += additional_fuel;
+                additional_fuel = additional_fuel / 3 - 2;
+            }
+            total_fuel += req_fuel;
+        }
+
+        Console.WriteLine($"Part 2: {total_fuel}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
--- AoCAll/Program.cs
+++ AoCAll/Program.cs
@@ -3,6 +3,7 @@ public class AoC {
     public static void Main(string[] args) {
         AoC2015.AoC.Main(args);
         AoC2018.AoC.Main(args);
+        AoC2019.AoC.Main(args);
         AoC2023.AoC.Main(args);
         AoC2024.AoC.Main(args);
     }
