f3201808b36701b943a8a03d791e3d87fc6e26ea
Zefyro <71092443+Zefyro@users.noreply.github.com>
2024-12-04 15:20:29 +0200

    AoC 2015 - Day 1

---
+++ AoC2015/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AoC2015;
+public class AoC {
+    public static void Main(string[] args) {
+        Day1(File.ReadAllText("AoC2015/inputs/day1.txt"));
+    }
+    public static void Day1(string input) {
+        Console.WriteLine("\nAdvent of Code 2015 - Day 1");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        
+        int floor = 0;
+        int basement_step = 0;
+        bool visited_basement = false;
+        
+        for (int i = 0; i < input.Length; i++) {
+            if (input[i] == '(') 
+                floor++;
+            else if (input[i] == ')')
+                floor--;
+            if (!visited_basement && floor == -1) {
+                basement_step = i + 1;
+                visited_basement = true;
+            }
+        }
+        
+        Console.WriteLine($"Part 1:  {floor}");
+        Console.WriteLine($"Part 2:  {basement_step}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
