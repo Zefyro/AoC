a5dd81df2614ef5f5e01d8481be41fa872dc95aa
Zefyro <zefyrogaming@gmail.com>
2025-12-02 10:00:58 +0200

    AoC 2025 - Day 2

--- AoC2025/Program.cs
+++ AoC2025/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace AoC2025;
+﻿using System.Transactions;
+
+namespace AoC2025;
 
 public class AoC
 {
     public static void Main()
     {
-        Day1(File.ReadAllText("./inputs/day1.txt"));
+        //Day1(File.ReadAllText("./inputs/day1.txt"));
+        Day2(File.ReadAllText("./inputs/day2.txt"));
     }
     public static void Day1(string input)
     {
@@ -47,6 +50,62 @@ public class AoC
         }
 
         Console.WriteLine($"Part 2: {zeros}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public static void Day2(string input)
+    {
+        Console.WriteLine("\nAdvent of Code 2025 - Day 2");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        string[] ranges = input.Split(',');
+
+        long invalid_id_sum = 0;
+
+        foreach (string range in ranges)
+        {
+            string[] values = range.Split('-');
+            for (long id = long.Parse(values[0]); id < long.Parse(values[1]); id++)
+            {
+                string string_id = id.ToString();
+                int half_length = string_id.Length / 2;
+                if (string_id[..half_length] == string_id[half_length..]) invalid_id_sum += id;
+            }
+        }
+
+        Console.WriteLine($"Part 1: {invalid_id_sum}");
+
+        invalid_id_sum = 0;
+
+        foreach (string range in ranges)
+        {
+            string[] values = range.Split('-');
+            for (long id = long.Parse(values[0]); id < long.Parse(values[1]); id++)
+            {
+                string string_id = id.ToString();
+                int half_length = string_id.Length / 2;
+                if (string_id[..half_length] == string_id[half_length..])
+                {
+                    invalid_id_sum += id;
+                    continue;
+                }
+                for (int length = 1; length < string_id.Length; length++)
+                {
+                    string substring = string_id[..length];
+                    string temp = substring;
+                    do
+                    {
+                        substring += temp;
+                    }
+                    while (string_id.Length > substring.Length);
+                    if (string_id == substring)
+                    {
+                        invalid_id_sum += id;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Part 2: {invalid_id_sum}");
         Console.ForegroundColor = ConsoleColor.White;
     }
 }
